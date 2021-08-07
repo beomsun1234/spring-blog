15d9ae65b4fd8750c5e46f0939d5aece16b32f3a
Beomsun <beomsun22@gmail.com>
2021-08-08 00:15:43 +0900

    ADD - PostServiceTest CASE

--- src/test/java/com/bs/springblog/SpringBlogApplicationTests.java
+++ src/test/java/com/bs/springblog/SpringBlogApplicationTests.java
@@ -3,8 +3,9 @@ package com.bs.springblog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "classpath:application-test.yml")
 class SpringBlogApplicationTests {
+
 
 	@Test
 	void contextLoads() {
--- src/test/java/com/bs/springblog/service/PostServiceTest.java
+++ src/test/java/com/bs/springblog/service/PostServiceTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -20,7 +22,7 @@ class PostServiceTest {
     private PostRepository postRepository;
 
     @Test
-    public void 게시글저장테스트(){
+    public void 게시글_저장_테스트(){
         //given
         Post post = postInit();
         //when
@@ -28,7 +30,45 @@ class PostServiceTest {
         //then
         Assertions.assertThat(postRepository.findById(findId)).isNotEmpty();
     }
+    @Test
+    public void 게시글_수정_테스트() throws Exception {
+        //given
+        Post post = postInit();
+        Long saveId = postService.save(post);
+        String title = "UpdateTest";
+        String content = "UpdateTest";
+        Post updateParam = Post.builder().title(title).content(content).build();
+        //when
+        Long updateId = postService.update(saveId, updateParam);
+        //then
+        Post updatedPost = postService.findOneById(updateId);
+        Assertions.assertThat(updatedPost.getTitle()).isEqualTo(title);
+    }
 
+    @Test
+    public void 게시글_한건_조회_테스트(){
+        //given
+        Post post = postInit();
+        Long saveId = postService.save(post);
+        //when
+        Post findPost = postService.findOneById(saveId);
+        //then
+        Assertions.assertThat(findPost.getId()).isEqualTo(saveId);
+    }
+    @Test
+    public void 게시글_여러건_조회_테스트(){
+        //given
+        Post post = postInit();
+        Post post2 = postInit();
+        Post post3 = postInit();
+        Long saveId = postService.save(post);
+        Long saveId2 = postService.save(post2);
+        Long saveId3 = postService.save(post3);
+        //when
+        List<Post> posts = postService.findAll();
+        //then
+        Assertions.assertThat(posts.size()).isEqualTo(3);
+    }
 
     public Post postInit(){
         String title = "test";
